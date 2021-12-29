21d38123742aeac55a669323b4ff1836853e6427
Rafiul Alam Antar <78463817+antarappnap@users.noreply.github.com>
2021-12-29 14:39:23 +0600

    Convert to maven

--- WidgetHub/src/Login.java
+++ WidgetHub/src/Login.java
@@ -1,11 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test Push");
-		
-
+	public static void main(String[] args) throws InterruptedException {
+		 
+		 
+	    // Start chrome browser
+	 
+	    System.setProperty("webdriver.chrome.driver", "/Users/appnapws20/Downloads/Automation/chromedriver");
+			WebDriver driver = new ChromeDriver();
+	 
+	    // start the application
+			driver.get("https://dev.wejet.app/APN040/Admin/Login");
 	}
-
 }
