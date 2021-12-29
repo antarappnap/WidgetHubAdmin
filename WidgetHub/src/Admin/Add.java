f52ea757cdca746d39a2cfd14f80193e68fdafe0
Rafiul Alam Antar <78463817+antarappnap@users.noreply.github.com>
2021-12-29 17:16:20 +0600

    Login automation implemented

--- WidgetHub/src/Admin/Add.java
+++ WidgetHub/src/Admin/Add.java
@@ -1,10 +1,26 @@
 package Admin;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Add {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) throws InterruptedException {
+		 
+		 
+	    // Start chrome browser
+	 
+	    System.setProperty("webdriver.chrome.driver", "/Users/appnapws20/Downloads/Automation/chromedriver");
+			WebDriver driver = new ChromeDriver();
+	 
+	    // start the application
+			driver.get("https://dev.wejet.app/APN040/Admin/Login");
+			driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("kabir@appnap.io");
+			driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("KabirWeJet21#");
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 
 }
--- WidgetHub/src/Login/Login.java
+++ WidgetHub/src/Login/Login.java
@@ -1,4 +1,5 @@
 package Login;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,5 +15,9 @@ public class Login {
 	 
 	    // start the application
 			driver.get("https://dev.wejet.app/APN040/Admin/Login");
+			driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("kabir@appnap.io");
+			driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("KabirWeJet21#");
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 }
