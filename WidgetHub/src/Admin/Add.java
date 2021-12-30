1662c7a69ee49dda5d02f7f3d567f6ce5a67943f
Rafiul Alam Antar <78463817+antarappnap@users.noreply.github.com>
2021-12-30 17:54:45 +0600

    click button using automation in add admin

--- WidgetHub/src/Admin/Add.java
+++ WidgetHub/src/Admin/Add.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class Add {
@@ -51,7 +52,7 @@ public class Add {
 		    driver.findElement(By.id("exampleInputPassword1")).sendKeys("KabirWeJet21#");
 		    Thread.sleep(2000);
 		    driver.findElement(By.xpath("//button[@type='submit']")).click();
-		    
+		
 	}
 }
 
